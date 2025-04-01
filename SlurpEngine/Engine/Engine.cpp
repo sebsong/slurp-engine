3223b9bad4ed9a2d0c3a3804606c71c2be309ce5
Sebastian Song <seby.song@gmail.com>
2025-03-31 23:59:11 -0700

    create a black window

--- SlurpEngine/Engine/Engine.cpp
+++ SlurpEngine/Engine/Engine.cpp
@@ -1,10 +1,104 @@
 #include <iostream>
-#include <ostream>
 #include <windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    PSTR lpCmdLine, int nCmdShow)
+LPCSTR WINDOW_CLASS_NAME = "SlurpEngineWindowClass";
+
+LRESULT CALLBACK WindowProc(HWND WindowHandle, UINT Message, WPARAM wParam, LPARAM lParam)
 {
-    MessageBox(0, "Slurp's Up", "Slurp Engine", MB_OK|MB_ICONINFORMATION);
+    LRESULT Result = 0;
+    
+
+    switch (Message)
+    {
+    case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("ACTIVATE\n");
+        }
+        break;
+    case WM_SIZE:
+        {
+            OutputDebugStringA("SIZE\n");
+        }
+        break;
+    case WM_DESTROY:
+        {
+            OutputDebugStringA("DESTROY\n");
+        }
+        break;
+    case WM_CLOSE:
+        {
+            OutputDebugStringA("CLOSE\n");
+            Result = DefWindowProcA(WindowHandle, Message, wParam, lParam);
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT PaintStruct;
+            HDC DeviceContext = BeginPaint(WindowHandle, &PaintStruct);
+            RECT PaintRect = PaintStruct.rcPaint;
+            PatBlt(
+                DeviceContext,
+                PaintRect.left,
+                PaintRect.top,
+                PaintRect.right - PaintRect.left,
+                PaintRect.bottom - PaintRect.top,
+                BLACKNESS
+            );
+            EndPaint(WindowHandle, &PaintStruct);
+            OutputDebugStringA("PAINT\n");
+        }
+        break;
+    default:
+        {
+            // OutputDebugStringA("DEFAULT\n");
+            Result = DefWindowProcA(WindowHandle, Message, wParam, lParam);
+        }
+        break;
+    }
+
+    return Result;
+};
+
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    PSTR lpCmdLine,
+    int nCmdShow
+)
+{
+    WNDCLASSA WindowClass = {};
+
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = WindowProc;
+    WindowClass.hInstance = hInstance;
+    WindowClass.lpszClassName = WINDOW_CLASS_NAME;
+
+    RegisterClassA(&WindowClass);
+
+    HWND WindowHandle = CreateWindowExA(
+        0,
+        WindowClass.lpszClassName,
+        "Slurp's Up!",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
+
+    if (!WindowHandle)
+    {
+        OutputDebugStringA("FAILED");
+        return 0;
+    }
+
+    MSG Message;
+    while (GetMessageA(&Message, nullptr, 0, 0))
+    {
+        TranslateMessage(&Message);
+        DispatchMessageA(&Message);
+    }
+
     return 0;
 }
--- SlurpEngine/misc/build.bat
+++ SlurpEngine/misc/build.bat
@@ -1,6 +1,12 @@
 @echo off
 
+SETLOCAL
+
+set libs=user32.lib gdi32.lib
+
 mkdir %slurp_dir%\build
 pushd %slurp_dir%\build
-cl -Zi ..\Engine\Engine.cpp user32.lib
+cl -Zi ..\Engine\Engine.cpp %libs%
 popd
+
+ENDLOCAL
