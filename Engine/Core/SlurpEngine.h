57ea51844a43347c13755bbdbe9ea77beb1cef39
Sebastian Song <seby.song@gmail.com>
2025-12-21 15:59:32 -0800

    remove slurp lib and compile everything together

---
+++ Engine/Core/SlurpEngine.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "Platform.h"
+#include "Timer.h"
+#include "JobRunner.h"
+#include "Asset.h"
+#include "DynamicDeclaration.h"
+#include "Input.h"
+#include "Audio.h"
+#include "EntityPipeline.h"
+#include "AudioPlayer.h"
+#include "Game.h"
+
+#if UNITY_BUILD
+
+/** Single translation unit, unity build **/
+
+// TODO: figure out if we can move these with the other unity build includes
+// ReSharper disable once CppUnusedIncludeDirective
+#include "SpinLock.cpp"
+// ReSharper disable once CppUnusedIncludeDirective
+#include "MemoryConstructs.cpp"
+
+#endif
+
+namespace slurp {
+    struct EngineSystems {
+        timer::Timer timer;
+        job::JobRunner jobRunner;
+        asset::AssetLoader assetLoader;
+        entity::EntityPipeline entityPipeline;
+        audio::AudioPlayer audioPlayer;
+    };
+
+    void init(
+        memory::MemoryArena& permanentMemory,
+        memory::MemoryArena& transientMemory,
+        const platform::PlatformDll& platformDll,
+        const render::RenderApi& renderApi,
+        bool isInitialized
+    );
+
+    void frameStart();
+
+    void handleInput(
+        const MouseState& mouseState,
+        const KeyboardState& keyboardState,
+        const GamepadState (&gamepadStates)[MAX_NUM_GAMEPADS]
+    );
+
+    void bufferAudio(const audio::AudioBuffer& buffer);
+
+    void updateAndRender(float dt);
+
+    void frameEnd();
+
+    void shutdown();
+}
